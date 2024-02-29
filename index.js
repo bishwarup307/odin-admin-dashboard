15f774c631f6b6841f2273cfb448e80d29a5de2c
bishwarup307 <15094759+bishwarup307@users.noreply.github.com>
2024-02-29 13:58:42 +0530

    Add data attributes to profile icons

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const navWithSearch = document.querySelector(".navbar-search");
 const searchInput = document.querySelector("#search-bar");
 const navbarBack = document.querySelector(".navbar-back");
 
+const teamIcons = document.querySelector(".team__icons");
+
 const navbarLinks = document.querySelector(".navbar-links");
 const hamburgerMenu = document.querySelector(".hamburger-nav-icon");
 
@@ -21,4 +23,19 @@ navbarBack.addEventListener("click", () => {
 hamburgerMenu.addEventListener("click", () => {
     console.log("clicked");
     navbarLinks.classList.toggle("expanded");
+});
+
+teamIcons.addEventListener("mouseover", (event) => {
+    event.stopPropagation();
+    targetDiv = event.target;
+    const name = targetDiv.dataset.name;
+
+    if (name) {
+        const tooltip = document.createDocumentFragment();
+
+        const p = document.createElement("p");
+        p.textContent = `Hi, ${name}`;
+        tooltip.appendChild(p);
+        targetDiv.appendChild(tooltip);
+    }
 });
